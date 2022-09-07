a74c19128d5e56993f727646ca6a556192a143be
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2022-09-07 12:12:16 +0100

    first code C

--- challenge_1/main.c
+++ challenge_1/main.c
@@ -3,16 +3,23 @@
 
 int main()
 {
-    char nom[10] , prenom[15];
+    char nom[10], prenom[15], sexe[1], number[13];
     int age;
     printf("Bonjour!\n Saisir votre nom : ");
     scanf("%s",&nom);
-    printf("Bonjour!\n Saisir votre prenom : ");
+    printf(" Saisir votre prenom : ");
     scanf("%s",&prenom);
-    printf("Bonjour!\n Saisir votre age : ");
+    printf(" Saisir votre age : ");
     scanf("%d",&age);
-    printf("Bonjour!\n Saisir votre prenom : ");
-    scanf("%s",&prenom);
-    printf("%s",nom);
+    printf(" Choisir votre sexe [M/F] : ");
+    scanf("%s",&sexe);
+    printf(" Saisir votre numero de telephone : ");
+    scanf("%s",&number);
+
+    printf("Votre nom est : %s\n",nom);
+    printf("Votre prenom est : %s\n",prenom);
+    printf("Votre age est : %d\n",age);
+    printf("Votre sexe est : %s\n",sexe);
+    printf("Votre numero de telephone est : %s \n",number);
     return 0;
 }
