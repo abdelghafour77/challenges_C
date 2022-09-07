ce58caa611be7ddfeb04a17ca940bf847d558af9
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2022-09-07 12:03:31 +0100

    first code C

---
+++ challenge_1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nom[10] , prenom[15];
+    int age;
+    printf("Bonjour!\n Saisir votre nom : ");
+    scanf("%s",&nom);
+    printf("Bonjour!\n Saisir votre prenom : ");
+    scanf("%s",&prenom);
+    printf("Bonjour!\n Saisir votre age : ");
+    scanf("%d",&age);
+    printf("Bonjour!\n Saisir votre prenom : ");
+    scanf("%s",&prenom);
+    printf("%s",nom);
+    return 0;
+}
