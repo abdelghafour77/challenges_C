3a385c61853c537d8c1064094cf6666802ac8dab
Abdelghafour AOUAD <abdelghafour.aouad0@gmail.com>
2022-09-14 15:11:08 +0100

    part 2 of WorkShop

--- Condition & Boucle/challenge_12/main.c
+++ Condition & Boucle/challenge_12/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void copy(char *str1, char *str2){
+void copy(char *str1[80], char *str2[80]){
 
     for(int i=0 ; i<=sizeof(str1) ; i++){
             str2[i] = str1[i];
@@ -9,7 +9,7 @@ void copy(char *str1, char *str2){
 }
 int main()
 {
-    char str1[20],str2[20];
+    char str1[80],str2[80];
     printf("type anything : ");
     scanf("%s",&str1);
 
